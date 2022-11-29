83fc641fc39d3cf18e5e525916954d6f6635188c
savate <gvzmxdj@gmail.com>
2022-11-29 14:26:23 -0300

    Modify Calendar to slash command

--- commands/ortho_calendar.rb
+++ commands/ortho_calendar.rb
@@ -1,22 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
 
-module Commands
-    extend Discordrb::Commands::CommandContainer
+module ApplicationCommands
+    extend Discordrb::EventContainer
+
     doc = Nokogiri::HTML(URI.open('https://www.oca.org/readings'))
     scripture = doc.css('section ul').map { |s| s.content }
     saints = doc.css('section p').map { |s| s.content }
+    date = doc.css('header h2').map { |d| d.content }
 
-    command :calendar do |event|
-        event.channel.send_embed do |embed|
-            embed.title = ':orthodox_cross: Orthodox Calendar'
-            embed.timestamp = Time.now
-            embed.add_field(name: 'Scripture Readings', value: [
-                "#{scripture.join('\t').gsub(/\t/, '')}"
-            ].join, inline: false)
-            embed.add_field(name: 'Today’s commemorated feasts and saints', value: [
-              "#{saints.join}"
-            ].join, inline: true)
+    application_command(:calendar) do |event|
+        event.respond do |builder|
+            builder.add_embed do |embed|
+                embed.title = ':orthodox_cross: Orthodox Calendar'
+                embed.description = "#{date.join}"
+                embed.timestamp = Time.now
+                embed.add_field(name: 'Scripture Readings', value: [
+                  "#{scripture.join('\t').gsub(/\t/, '')}"
+                ].join, inline: false)
+                embed.add_field(name: 'Today’s commemorated feasts and saints', value: [
+                  "#{saints.join}"
+                ].join, inline: true)
+            end
         end
     end
 end
--- slash_registers/registers.rb
+++ slash_registers/registers.rb
@@ -7,5 +7,5 @@ TOKEN = ENV['TOKEN']
 bot = Discordrb::Bot.new(token: TOKEN, intents: [:server_messages])
 
 bot.register_application_command(:invite, "Ricky's invite URL")
-
+bot.register_application_command(:calendar, 'Scripture readings')
 bot.run
