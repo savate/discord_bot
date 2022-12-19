f75345e9b1d78702d45b40da348d2e234253b59e
savate <gvzmxdj@gmail.com>
2022-12-19 10:18:19 -0300

    Remove unecessery line

--- commands/user_avatar.rb
+++ commands/user_avatar.rb
@@ -8,7 +8,6 @@ module ApplicationCommands
   bot = Discordrb::Bot.new(token: TOKEN, intents: [:server_messages])
 
   application_command(:user).subcommand('avatar') do |event|
-    puts event.command_id
     user = event.options.empty? ? event.user : bot.user(event.options['user'].to_s)
     url = "#{user.avatar_url}?size=1024"
 
@@ -19,4 +18,5 @@ module ApplicationCommands
       end
     end
   end
-end
+  end
+1054241601242800189
