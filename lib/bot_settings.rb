270e1343c59fc373a1b637037a483f63470890e1
savate <gvzmxdj@gmail.com>
2022-12-19 11:47:31 -0300

    Change settings to get unique instance of the bot

--- lib/bot_settings.rb
+++ lib/bot_settings.rb
@@ -1,9 +1,10 @@
 require 'discordrb'
 
-module Initialize
-  class Bot < Discordrb::Commands::CommandBot
-    def initialize(token, prefix)
-      super(token: token, prefix: prefix)
-    end
+module BotSettings
+  Dotenv.load
+  TOKEN = ENV['TOKEN']
+  def get_bot
+    bot = Discordrb::Bot.new(token: TOKEN, intents: [:server_messages])
+    bot
   end
 end
