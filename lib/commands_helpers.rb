61c138e107849d2efe96e577b344e417e7232cf4
savate <gvzmxdj@gmail.com>
2022-12-20 16:32:11 -0300

    Break space for readability

--- lib/commands_helpers.rb
+++ lib/commands_helpers.rb
@@ -5,7 +5,7 @@ module CommandsHelpers
   def query_formatter(query)
     query_string = query.to_s
     request = HTTParty.get("https://youtube.googleapis.com/youtube/v3/search?ma
-                            xResults=1&order=viewCount&q=#{query_string}&key=#{YOUTUBE_KEY}")
+                            xResults=1&part=snippet&order=viewCount&q=#{query_string}&key=#{YOUTUBE_KEY}")
     if request.code == 200
       request_parsed = JSON.parse(request.body)
       video_id = request_parsed['items'][0]['id']['videoId']
