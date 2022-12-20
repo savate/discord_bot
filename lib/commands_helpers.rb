f8ff933fc0c346943d9884500dd9563251488bea
savate <gvzmxdj@gmail.com>
2022-12-20 18:08:05 -0300

    Update commands helpers to search for another video

--- lib/commands_helpers.rb
+++ lib/commands_helpers.rb
@@ -2,13 +2,13 @@ module CommandsHelpers
   Dotenv.load
   YOUTUBE_KEY = ENV['YOUTUBE_KEY']
 
-  def query_formatter(query)
+  def query_formatter(query, i)
     query_string = query.to_s
     request = HTTParty.get("https://youtube.googleapis.com/youtube/v3/search?ma
                             xResults=1&part=snippet&order=viewCount&q=#{query_string}&key=#{YOUTUBE_KEY}")
     if request.code == 200
       request_parsed = JSON.parse(request.body)
-      video_id = request_parsed['items'][0]['id']['videoId']
+      video_id = request_parsed['items'][i]['id']['videoId']
       res = "https://www.youtube.com/watch?v=#{video_id}"
       res
     else
