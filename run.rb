b0c52b3377c8eee869ec1e60edb8cf7453275bac
savate <gvzmxdj@gmail.com>
2022-12-22 16:05:59 -0300

    Change lib require to dir loop

--- run.rb
+++ run.rb
@@ -1,11 +1,13 @@
 require 'discordrb'
 require 'dotenv'
-require './lib/bot_settings'
+Dir['./lib/*.rb'].each { |file| require file }
 Dir['./commands/*.rb'].each { |file| require file }
+load 'keeponline.rb'
 
 include BotSettings
 
 bot = get_bot
 bot.include! ApplicationCommands
 
+keeponline()
 bot.run
