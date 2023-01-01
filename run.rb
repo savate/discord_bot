dfddaae0b2d24a41d6f3f5ea38d4b753905a4c22
savate <gvzmxdj@gmail.com>
2023-01-01 15:52:12 -0300

    Remove call to keeponline function

--- run.rb
+++ run.rb
@@ -2,12 +2,10 @@ require 'discordrb'
 require 'dotenv'
 Dir['./lib/*.rb'].each { |file| require file }
 Dir['./commands/*.rb'].each { |file| require file }
-load 'keeponline.rb'
 
 include BotSettings
 
 bot = get_bot
 bot.include! ApplicationCommands
 
-keeponline()
 bot.run
